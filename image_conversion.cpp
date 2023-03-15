05cd4cb610ac6c050aaaf58abcee2b4ce24d80a4
git-for-jobs-123 <->
2023-03-15 17:46:15 +0200

    Make YUV420888 to RGB image conversion faster

--- image_conversion.cpp
+++ image_conversion.cpp
@@ -2,22 +2,16 @@
 // Bug: fails to convert green color
 void YUV420888_to_RGB(const char* y, const char* u, const char* v, unsigned char* rgb, int width, int height, int y_pixel_stride, int y_row_stride, int uv_pixel_stride, int uv_row_stride) {
   for (int y_coord = 0; y_coord < height; y_coord++) {
+    int y_idx = y_coord * y_row_stride;
+    int uv_idx = (y_coord >> 1) * uv_row_stride;
     for (int x_coord = 0; x_coord < width; x_coord++) {
-      // Compute the index for the Y channel
-      int y_idx = y_coord * y_row_stride + x_coord * y_pixel_stride;
-
-      // Compute the index for the U and V channels
-      int uv_idx = (y_coord >> 1) * uv_row_stride + (x_coord >> 1) * uv_pixel_stride;
-      int u_idx = uv_idx;
-      int v_idx = uv_idx + 1;
-
       // Extract the Y, U, and V values
-      int Y = y[y_idx] & 0xff;
-      int U = u[u_idx] & 0xff;
-      int V = v[v_idx] & 0xff;
+      int Y = y[y_idx + x_coord * y_pixel_stride] & 0xff;
+      int U = u[uv_idx + (x_coord >> 1) * uv_pixel_stride] & 0xff;
+      int V = v[uv_idx + (x_coord >> 1) * uv_pixel_stride + 1] & 0xff;
 
       int r = Y + (1.370705 * (V - 128));
-      int g = Y - (0.698001 * (V - 128)) - (0.337633 * (U - 128));;
+      int g = Y - (0.698001 * (V - 128)) - (0.337633 * (U - 128));
       int b = Y + (1.732446 * (U - 128));
 
       // Clamp the R, G, and B values to the range [0, 255]
